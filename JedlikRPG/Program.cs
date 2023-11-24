64a2c9dd52a84190197a54d5ee10900e23e47208
Kiss-Martin <kiss.martin@students.jedlik.eu>
2023-11-24 11:52:28 +0100

    kezdokepernyo

--- JedlikRPG/Program.cs
+++ JedlikRPG/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace JedlikRPG
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JedlikRPG
 {
     internal partial class Program
     {
+        string command;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("   __     ______     _____     __         __     __  __     ______     ______   ______    \r\n  /\\ \\   /\\  ___\\   /\\  __-.  /\\ \\       /\\ \\   /\\ \\/ /    /\\  == \\   /\\  == \\ /\\  ___\\   \r\n _\\_\\ \\  \\ \\  __\\   \\ \\ \\/\\ \\ \\ \\ \\____  \\ \\ \\  \\ \\  _\"-.  \\ \\  __<   \\ \\  _-/ \\ \\ \\__ \\  \r\n/\\_____\\  \\ \\_____\\  \\ \\____-  \\ \\_____\\  \\ \\_\\  \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\_\\    \\ \\_____\\ \r\n\\/_____/   \\/_____/   \\/____/   \\/_____/   \\/_/   \\/_/\\/_/   \\/_/ /_/   \\/_/     \\/_____/ \r\n");
+            Console.Write("\nÍrd be a karaktered nevét: ");
+
+            string név = Console.ReadLine();
+
+            Console.WriteLine("A karaktered neve: " + név);
         }
+
     }
 }
