cb2daff0ea5eed16bce8197ac81a8ea57ad37979
HUNBenjamin <reinhardt.benjamin@students.jedlik.eu>
2023-11-24 13:10:57 +0100

    debug mara ennyi mo

--- JedlikRPG/Program.cs
+++ JedlikRPG/Program.cs
@@ -20,7 +20,6 @@ namespace JedlikRPG
             string név = Console.ReadLine();
 
             Console.WriteLine("A karaktered neve: " + név);
-=======
             int alvas = 6;
             int busz = 30;
             int elegemvan = 0;
@@ -29,6 +28,7 @@ namespace JedlikRPG
             int ero = 0;
             bool gameover = false;
 
+            Console.WriteLine($"Helyszín: Otthon ");
             Console.WriteLine($"Idő: ");
             Console.WriteLine($"Elegem van: {elegemvan}");
             Console.WriteLine($"Éhség: {ehseg}");
@@ -44,67 +44,7 @@ namespace JedlikRPG
 
             while(gameover != true)
             {
-                while (choice < 1 || choice > 3)
-                {
-                    choice = Input("Választás: ");
-                    if (choice == 1)
-                    {
-                        Console.Clear();
-
-                        Console.WriteLine($"Idő: ");
-                        Console.WriteLine($"Elegem van: {elegemvan}");
-                        Console.WriteLine($"Éhség: {ehseg}");
-                        Console.WriteLine($"Húgyhólyag állapota: {hugyholyag}");
-                        Console.WriteLine($"Erő: {ero}\n\n");
-
-                        Console.WriteLine("\nElmentél vizelni egy jót");
-                        hugyholyag = 0;
-                        busz -= 2;
-                        Console.WriteLine($"Készülj el, a busz {busz} perc múlva érkezik");
-                        Console.WriteLine("");
-                        Console.WriteLine("2 - Elfogyasztod a reggelid (15 perc)\n3 - Elmész a buszmegállóba (3 perc)");
-                        choice = 0;
-                    }
-                    if (choice == 2)
-                    {
-                        Console.Clear();
-
-                        Console.WriteLine($"Idő: ");
-                        Console.WriteLine($"Elegem van: {elegemvan}");
-                        Console.WriteLine($"Éhség: {ehseg}");
-                        Console.WriteLine($"Húgyhólyag állapota: {hugyholyag}");
-                        Console.WriteLine($"Erő: {ero}\n\n");
-
-                        Console.WriteLine("\nMég életedben nem ettél finomat. Bár a házi koszt nem csillapítja az elégedetlenséged, melyet iskolád okoz");
-                        ehseg -= 25;
-                        busz -= 15;
-                        Console.WriteLine($"Készülj el, a busz {busz} perc múlva érkezik");
-                        Console.WriteLine("");
-                        Console.WriteLine("1 - Elmész vizelni (2 perc)\n2 - Elmész a buszmegállóba (3 perc)");
-                        choice = 0;
-                    }
-                    if (choice == 3)
-                    {
-                        Console.Clear();
-
-                        Console.WriteLine($"Idő: ");
-                        Console.WriteLine($"Elegem van: {elegemvan}");
-                        Console.WriteLine($"Éhség: {ehseg}");
-                        Console.WriteLine($"Húgyhólyag állapota: {hugyholyag}");
-                        Console.WriteLine($"Erő: {ero}\n\n");
-
-                        busz -= 3;
-                        Console.WriteLine($"Kisétáltál a buszmegállóba, a járatod {busz} perc múlva érkezik");
-                    }
-                    if (busz < 0)
-                    {
-                        Console.WriteLine("\nSajnos lekésted a buszt.");
-                        Console.WriteLine("Nem sikerült beérned az iskolába, így túl sok hiányzás miatt kirúgtak.");
-                        gameover = true;
-                        Console.WriteLine("A játéknak vége\n");
-                        break;
-                    }
-                }
+                otthon(choice, busz, elegemvan, ehseg, hugyholyag, ero, gameover);
             }
             
         }
