9087e42a56e334febaac6dd1551e2774798d3091
Kiss-Martin <kiss.martin@students.jedlik.eu>
2023-12-04 10:30:27 +0100

    semmi

---
+++ JedlikRPG/itemek.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Item
+{
+    public string Name = "Taknyos zsebkendő";
+    public string Description = "Taknyos.";
+
+    string[] Items = { "Kaja", "Alkohol", "Pelenka" };
+    string[] Descriptions = { "Egyszerű pékáru ami csökkenti az éhséget.", "A subidubi segíti általános jókedvünk megőrzését", "A nap folyamán nem kell hugyoznod" };
+    int[] Elegemvan = { 0, -20, 0 };
+    int[] Hugyholyag = { 0, 25, 0 };
+    int[] Erő = { 5, 5, 0 };
+}
+//struct Fogyaszthato Kaja = Fogyaszthato("Kaja", "Egyszerű pékáru.", 0, 0, 5, "Átlagos");
+//struct Fogyaszthato Alkohol = Fogyaszthato("Alkohol", "Subidubi hohó.", -20, 25, 5, "Átlagos");
