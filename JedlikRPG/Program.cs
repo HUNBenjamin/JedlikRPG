5390a0c4018bce30e77b6670cdb6600d2fcffb18
HUNBenjamin <reinhardt.benjamin@students.jedlik.eu>
2023-12-06 22:53:57 +0100

    elegem van == 100;

--- JedlikRPG/Program.cs
+++ JedlikRPG/Program.cs
@@ -23,9 +23,9 @@ namespace JedlikRPG
             Console.ForegroundColor = ConsoleColor.Blue;
 
             Console.WriteLine("   __     ______     _____     __         __     __  __     ______     ______   ______    \r\n  /\\ \\   /\\  ___\\   /\\  __-.  /\\ \\       /\\ \\   /\\ \\/ /    /\\  == \\   /\\  == \\ /\\  ___\\   \r\n _\\_\\ \\  \\ \\  __\\   \\ \\ \\/\\ \\ \\ \\ \\____  \\ \\ \\  \\ \\  _\"-.  \\ \\  __<   \\ \\  _-/ \\ \\ \\__ \\  \r\n/\\_____\\  \\ \\_____\\  \\ \\____-  \\ \\_____\\  \\ \\_\\  \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\_\\    \\ \\_____\\ \r\n\\/_____/   \\/_____/   \\/____/   \\/_____/   \\/_/   \\/_/\\/_/   \\/_/ /_/   \\/_/     \\/_____/ \r\n");
-         
+
             Console.ForegroundColor = ConsoleColor.White;
-            
+
             Console.Write("1 - Könnyű\n2 - Közepes\n3 - Nehéz\n4 - Extrém\nAdd meg a nehézségi szintet: ");
             int nehezseg = int.Parse(Console.ReadLine());
             Difficulty difficulty = new Difficulty();
@@ -34,16 +34,17 @@ namespace JedlikRPG
             int busz = difficulty.Busz;
             int osztondij = difficulty.Osztondij;
             double szorzo = difficulty.Szorzo;
+            
             int elegemvan = 0;
             int ehseg = 50;
             int hugyholyag = 80;
             int ero = 0;
+            int becsengo = 30;
             bool buszjegy = false;
             bool gameover = false;
             List<string> Inventory = new List<string>();
 
             Console.WriteLine($"Helyszín: Otthon ");
-            Console.WriteLine($"Idő: ");
             Console.WriteLine($"Elegem van: {elegemvan}");
             Console.WriteLine($"Éhség: {ehseg}");
             Console.WriteLine($"Húgyhólyag állapota: {hugyholyag}");
@@ -55,35 +56,35 @@ namespace JedlikRPG
 
             Console.WriteLine("1 - Elmész vizelni (2 perc)\n2 - Elfogyasztod a reggelid (15 perc)\n3 - Elmész a buszmegállóba (3 perc)\n4 - Táska megtekintése");
             int choice = 0;
+            int choice2 = 0;
 
-            while(gameover != true)
-            {
                 if (reggel(choice, busz, elegemvan, ehseg, hugyholyag, ero, Inventory, gameover, buszjegy, out choice, out busz, out elegemvan, out ehseg, out hugyholyag, out ero, out Inventory, out gameover, out buszjegy) == 1)
+                {
+                    gameover = true;
+                }
+
+                megallo(choice, busz, elegemvan, ehseg, hugyholyag, ero, Inventory, gameover, buszjegy, out choice, out busz, out elegemvan, out ehseg, out hugyholyag, out ero, out Inventory, out gameover, out buszjegy);
+
+                blicceles(choice, busz, elegemvan, ehseg, hugyholyag, ero, buszjegy, osztondij, out osztondij);
+                if (blicceles(choice, busz, elegemvan, ehseg, hugyholyag, ero, buszjegy, osztondij, out osztondij) == 1)
+                {
+                    osztondij -= 8000;
+                    Console.WriteLine("Leszálltál a buszról, miután megérkeztél a megállóba. A város látványa mindig szomorúvá tesz, azonban most különösen letaglózónak érzed ezt a helyet, mely egykor szívedet melengette, de már csak az üresség árad belőle.");
+                }
+                else
+                {
+                    Console.WriteLine("Leszálltál a buszról, miután megérkeztél a megállóba. A város látványa mindig szomorúvá tesz, azonban most különösen letaglózónak érzed ezt a helyet, mely egykor szívedet melengette, de már csak az üresség árad belőle.");
+                }
+
+                if (szentistvanut(choice2, busz, elegemvan, ehseg, hugyholyag, ero, Inventory, gameover, buszjegy, becsengo, out choice2, out busz, out elegemvan, out ehseg, out hugyholyag, out ero, out Inventory, out gameover, out buszjegy, out becsengo) == 1)
                 {
                     gameover = true;
                 }
                 else
                 {
-                    if (blicceles(choice, busz, elegemvan, ehseg, hugyholyag, ero, buszjegy) == 1)
-                    {
-                        osztondij -= 8000;
-                        Console.WriteLine("Leszálltál a buszról, miután megérkeztél a megállóba. A város látványa mindig szomorúvá tesz, azonban most különösen letaglózónak érzed ezt a helyet, mely egykor szívedet melengette, de már csak a gyári naplementét bámulod melankóliában.");
-                        break;
-
-                        Console.WriteLine("Kinyitva a táskád ezt találod:");
-                        foreach (string Name in Item.Names)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.WriteLine(Name);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Leszálltál a buszról, miután megérkeztél a megállóba. A város látványa mindig szomorúvá tesz, azonban most különösen letaglózónak érzed ezt a helyet, mely egykor szívedet melengette, de már csak a gyári naplementét bámulod melankóliában.");
-                        break;
-                    }
+                    Console.WriteLine("Placeholder");
                 }
-            }
+            
         }
     }
 }
--- JedlikRPG/reggel.cs
+++ JedlikRPG/reggel.cs
@@ -37,28 +37,6 @@ namespace JedlikRPG
                 {
                     busz -= 3;
                     buszSeta(choice, busz, elegemvan, ehseg, hugyholyag, ero);
-                    choice = 0;
-                    while (choice < 1 || choice > 2)
-                    {
-                        Console.WriteLine($"1 - Vásárolj jegyet (2 perc)\n2 - Várd meg a buszt ({busz} perc)");
-                        choice = Input("Választás: ");
-                        if (choice == 1)
-                        {
-                            busz -= 2;
-                            buszjegy = true;
-                            jegyvetel(choice, busz, elegemvan, ehseg, hugyholyag, ero);
-                            choice = 0;
-                        }
-                        else
-                        {
-                            while (busz > 0)
-                            {
-                                busz--;
-                            }
-                            buszraszallas(choice, busz, elegemvan, ehseg, hugyholyag, ero);
-                            break;
-                        }
-                    }
 
                     if (busz < 0)
                     {
@@ -105,6 +83,142 @@ namespace JedlikRPG
             x7 = Inventory;
             x8 = gameover;
             x9 = buszjegy;
+            return 0;
+        }
+
+        static int megallo(int choice, int busz, int elegemvan, int ehseg, int hugyholyag, int ero, List<string> Inventory, bool gameover, bool buszjegy, out int x1, out int x2, out int x3, out int x4, out int x5, out int x6, out List<string> x7, out bool x8, out bool x9)
+        {
+            while (choice < 1 || choice > 2)
+            {
+                Console.WriteLine($"1 - Vásárolj jegyet (2 perc)\n2 - Várd meg a buszt ({busz} perc)");
+                choice = Input("Választás: ");
+                if (choice == 1)
+                {
+                    busz -= 2;
+                    buszjegy = true;
+                    jegyvetel(choice, busz, elegemvan, ehseg, hugyholyag, ero);
+                    choice = 0; 
+                }
+                else
+                {
+                    while (busz >= 0)
+                    {
+                        busz--;
+                    }
+                    buszraszallas(choice, busz, elegemvan, ehseg, hugyholyag, ero);
+                }
+            }
+            if (busz < 0)
+            {
+                Console.WriteLine("\nSajnos lekésted a buszt.");
+                Console.WriteLine("Nem sikerült beérned az iskolába, így túl sok hiányzás miatt kirúgtak.");
+                gameover = true;
+                Console.WriteLine("\nA játéknak vége\n");
+            }
+
+            x1 = choice;
+            x2 = busz;
+            x3 = elegemvan;
+            x4 = ehseg;
+            x5 = hugyholyag;
+            x6 = ero;
+            x7 = Inventory;
+            x8 = gameover;
+            x9 = buszjegy;
+            return 0;
+        }
+
+        static int szentistvanut(int choice, int busz, int elegemvan, int ehseg, int hugyholyag, int ero, List<string> Inventory, bool gameover, bool buszjegy, int becsengo, out int x1, out int x2, out int x3, out int x4, out int x5, out int x6, out List<string> x7, out bool x8, out bool x9, out int x10)
+        {
+            choice = 0;
+            while (choice < 1 || choice > 4)
+            {
+                Console.Clear();
+
+                Console.WriteLine($"Helyszín: Szent István út");
+                Console.WriteLine($"Elegem van: {elegemvan}");
+                Console.WriteLine($"Éhség: {ehseg}");
+                Console.WriteLine($"Húgyhólyag állapota: {hugyholyag}");
+                Console.WriteLine($"Erő: {ero}\n\n");
+                Console.WriteLine("\nMegérkeztél a városba. ");
+                Console.WriteLine($"Menj a Jedlikbe, {becsengo} perc múlva becsengetnek.");
+                Console.WriteLine("");
+
+
+                Console.WriteLine("\n1 - Lelépsz az úttestre a közeledő kamion elé\n2 - Bemész az euróboltba (5 perc)\n3 - Elsétálsz a Jedlikbe\n4 - Táska megtekintése");
+                choice = Input("Választás: ");
+                if (choice == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Helyszín: Szent István út");
+                    Console.WriteLine($"Elegem van: {elegemvan}");
+                    Console.WriteLine($"Éhség: {ehseg}");
+                    Console.WriteLine($"Húgyhólyag állapota: {hugyholyag}");
+                    Console.WriteLine($"Erő: {ero}\n\n");
+                    Console.WriteLine("");
+                    Console.WriteLine("Nem úszod meg ilyen könnyen");
+                    Console.WriteLine("\n1 - Lelépsz az úttestre a közeledő kamion elé\n2 - Bemész az euróboltba (5 perc)\n3 - Elsétálsz a Jedlikbe (5 perc)\n4 - Táska megtekintése");
+                    choice = 0;
+                }
+                if (choice == 2)
+                {
+                    //Eurobolt(choice, elegemvan, ehseg, hugyholyag, ero, gameover, Inventory);
+                    Console.Clear();
+                    Console.WriteLine($"Helyszín: Szent István út");
+                    Console.WriteLine($"Elegem van: {elegemvan}");
+                    Console.WriteLine($"Éhség: {ehseg}");
+                    Console.WriteLine($"Húgyhólyag állapota: {hugyholyag}");
+                    Console.WriteLine($"Erő: {ero}\n\n");
+                    Console.WriteLine("");
+                    Console.WriteLine("Mennyi minden volt itt");
+                    Console.WriteLine("\n1 - Lelépsz az úttestre a közeledő kamion elé\n2 - Bemész az euróboltba (5 perc)\n3 - Elsétálsz a Jedlikbe (5 perc)\n4 - Táska megtekintése");
+                    choice = 0;
+                    becsengo -= 5;
+                }
+                if (choice == 3)
+                {
+                    Console.WriteLine("Megérkeztél iskoládba. Íme, ezt nevezik pokolnak.");
+                    becsengo -= 5;
+                    break;
+                }
+                if (becsengo < 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Helyszín: Jedlik");
+                    Console.WriteLine($"Elegem van: {elegemvan}");
+                    Console.WriteLine($"Éhség: {ehseg}");
+                    Console.WriteLine($"Húgyhólyag állapota: {hugyholyag}");
+                    Console.WriteLine($"Erő: {ero}\n\n");
+                    Console.WriteLine("");
+                    Console.WriteLine("\nSajnos túl sokáid vánszorogtál.");
+                    Console.WriteLine("Nem sikerült beérned az iskolába, így túl sok hiányzás miatt kirúgtak.");
+                    gameover = true;
+                    Console.WriteLine("\nA játéknak vége\n");
+                    x1 = choice;
+                    x2 = busz;
+                    x3 = elegemvan;
+                    x4 = ehseg;
+                    x5 = hugyholyag;
+                    x6 = ero;
+                    x7 = Inventory;
+                    x8 = gameover;
+                    x9 = buszjegy;
+                    x10 = becsengo;
+                    return 1;
+                }
+
+            }
+
+            x1 = choice;
+            x2 = busz;
+            x3 = elegemvan;
+            x4 = ehseg;
+            x5 = hugyholyag;
+            x6 = ero;
+            x7 = Inventory;
+            x8 = gameover;
+            x9 = buszjegy;
+            x10 = becsengo;
             return 0;
         }
     }
